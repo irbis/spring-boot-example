d2b9c4e073e4e6bdf03ef006c56018898c7eedbb
Alexsey Nadtochey <A.Nadtochey@gmail.com>
2016-06-05 15:10:46 +0300

    'application.properties' has been added.
    Default properties file 'application.properties' has been added to the
    project. As an example of usage properties defined in the
    application.properities ExampleController has been modified to read
    message from application.properties.

--- src/main/java/irbis/springboot/web/ExampleController.java
+++ src/main/java/irbis/springboot/web/ExampleController.java
@@ -1,13 +1,17 @@
 package irbis.springboot.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
 
+	@Value("${messages.helloworld}")
+	private String helloWorldMessage;
+	
     @RequestMapping("/")
     String home() {
-        return "Hello world!";
+        return helloWorldMessage;
     }
 }
