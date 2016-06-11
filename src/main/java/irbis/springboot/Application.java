17b083e4d66bee2a9515c244cb2f4ce271360fef
Alexsey Nadtochey <A.Nadtochey@gmail.com>
2016-06-11 16:12:04 +0300

    Simple page 'About Us' has been added.
    This page is out of restricted security context and available for all
    users without authorization.

--- src/main/java/irbis/springboot/Application.java
+++ src/main/java/irbis/springboot/Application.java
@@ -23,14 +23,21 @@ public class Application extends WebMvcConfigurerAdapter {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().anyRequest().fullyAuthenticated().and()
+			http
+				.authorizeRequests()
+				.antMatchers("/aboutus").permitAll()
+				.anyRequest().fullyAuthenticated()
+				.and()
 			.formLogin()
 				.loginPage("/login")
 				.loginProcessingUrl("/login")
-				.failureUrl("/login?error").permitAll().and()
-			.logout().
-				logoutUrl("/logout")
-				.logoutSuccessUrl("/login?logout").permitAll();
+				.failureUrl("/login?error")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login?logout")
+				.permitAll();
 		}
 
 		@Override
---
+++ src/main/java/irbis/springboot/web/AboutUsController.java
@@ -0,0 +1,12 @@
+package irbis.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AboutUsController {
+	@RequestMapping("/aboutus")
+	public String aboutus() {
+		return "aboutus";
+	}
+}
